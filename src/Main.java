f6ad006ef78067ed86ca7507b3e32b5295c12abe
Vladyslav-Kravtsov <vladyslav.kravtsov@student.karazin.ua>
2024-03-17 18:37:53 +0100

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        new GUI(); // Запуск GUI
+        System.out.println("Завдання №1");
+        new GUI();
         System.out.print("Введіть клас: ");
         String Class = scanner.next();
         Console.printClassDescription(Class); // Виклик аналізу через консоль
+        System.out.println("\n\n");
 
-        Check obj = new Check(3.0, 4.0);
-        Task2.inspect(obj);
+        System.out.println("Завдання №2");
+        Check obj2 = new Check(3.0, 4.0);
+        Task2.inspect(obj2);
+        System.out.println("\n\n");
+
+        System.out.println("Завдання №3");
+        Task3 obj3 = new Task3();
+        try {
+            Task3.callMethod(obj3, "func", new Task3.MethodParameter(double.class, 1.0));
+            Task3.callMethod(obj3, "pow", new Task3.MethodParameter(double.class, 1.0),
+                    new Task3.MethodParameter(int.class, 0));
+        } catch (FunctionNotFoundException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+        System.out.println("\n\n");
+
     }
 }
---
+++ src/Task3.java
@@ -0,0 +1,55 @@
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+class FunctionNotFoundException extends Exception {
+    public FunctionNotFoundException(String message) {
+        super(message);
+    }
+}
+class Task3 {
+    static class MethodParameter {
+        private final Class<?> type;
+        private final Object value;
+
+        public MethodParameter(Class<?> type, Object value) {
+            this.type = type;
+            this.value = value;
+        }
+
+        public Class<?> getType() {
+            return type;
+        }
+
+        public Object getValue() {
+            return value;
+        }
+    }
+    public static void callMethod(Object obj, String methodName, MethodParameter... params) throws FunctionNotFoundException {
+        Class<?>[] paramTypes = new Class<?>[params.length];
+        Object[] paramValues = new Object[params.length];
+
+        for (int i = 0; i < params.length; i++) {
+            MethodParameter param = params[i];
+            paramTypes[i] = param.getType();
+            paramValues[i] = param.getValue();
+        }
+
+        try {
+            Method method = obj.getClass().getMethod(methodName, paramTypes);
+            Object result = method.invoke(obj, paramValues);
+            System.out.println("Результат виклику методу " + methodName + ":" + result);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            throw new FunctionNotFoundException("Метод " + methodName + " не знайдено або не може бути викликаний.");
+        }
+    }
+
+    public double func(double a) {
+            return Math.exp(-Math.abs(a) * x) * Math.sin(x);
+        }
+
+        public double pow(double a, int b) {
+            return Math.pow(a, b) + x;
+        }
+
+        private final double x = 1.0;
+    }
